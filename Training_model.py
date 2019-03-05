8a1cbf53432308d54e2fcc536b5c63af5b2ec548
Omer Awan <mawan5512@gmail.com>
2019-03-05 11:10:13 -0500

    Commenting Training.py

--- Training_model.py
+++ Training_model.py
@@ -6,21 +6,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from joblib import dump, load
 
-
+#Reads in data into a 2D array, Includes Filenames, RGB Values, and the class labels
 data = pd.read_csv('/Users/mohammedawan/PycharmProjects/StegModel/sample_trainingset.csv').values
+#Gets number of dimensions
 dim = len(data[0])
+
+#Splits data into class labels, Y, and the X (Filenames, RGB values)
 Y = data[:,dim-1:].ravel().astype('int')
 X = data[:,:dim-1]
+
+#Splitst into testing and training data
 train_x, test_x, train_y, test_y = train_test_split(X, Y)
+
+#Seperate Filenames from RGB Values
 filenames_test = test_x[:,:1]
+#Test Data(RGB)
 data_test = test_x[:,1:]
+#Training Data(RGB)
 data_train = train_x[:,1:]
 
+#Defines the model and parameteres
 clf = RandomForestClassifier(max_depth=10, criterion='entropy')
+#Fit the model to the data
 clf.fit(data_train,train_y)
 
+#Creates file with saved model
 dump(clf, '/Users/mohammedawan/PycharmProjects/StegModel/stemodel.joblib')
+#Loads model from file
 clf1 = load('/Users/mohammedawan/PycharmProjects/StegModel/stemodel.joblib')
+#Runs a prediction on testing data
 prediction = clf1.predict(data_test)
-
+#Gives the accuracy of prediction from model
 print(accuracy_score(test_y, prediction))
