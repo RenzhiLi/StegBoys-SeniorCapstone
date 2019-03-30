4ae32f05251685b689893f56af5b832b4314e8ce
RenzhiLi <43355303+RenzhiLi@users.noreply.github.com>
2019-03-30 09:23:54 -0400

    Add files via upload

--- Preprocessing.py
+++ Preprocessing.py
@@ -7,18 +7,22 @@ import sys
 filetype = "dir"
 filextends = ('.jpg','.jpeg','.exif','.gif','.bmp','.png','.bpg',
 	'.tif')
+alps = 'etaino shrdlucmfwygpbv'
+alpchars = []
+for alp in alps:
+	alpchars.append((bin(ord(alp))[2:],bin(ord(alp.upper()))[2:]))
+
 def load_pics(directory : 'images directory') -> "a loaded pic list":
 	global filetype
 	im , file_paths = [], []
 	if os.path.isdir(directory):
 		for root,dirnames,filenames in os.walk(directory):
-			if root == directory:
-				for filename in filenames:
-					if filename.lower().endswith(filextends):
-						#re.search(r'\.jpg',filename)
-						filepath = os.path.join(root, filename)
-						file_paths.append(filepath)
-						im.append(Image.open(filepath))
+			for filename in filenames:
+				if filename.lower().endswith(filextends):
+					#re.search(r'\.jpg',filename)
+					filepath = os.path.join(root, filename)
+					file_paths.append(filepath)
+					im.append(Image.open(filepath))
 	elif os.path.isfile(directory):
 		im.append(Image.open(directory))
 		file_paths.append(directory)
@@ -47,7 +51,7 @@ def preprocess(data : 'loaded pic list (from load_pics())',
 	leave blank for the original format (3/4-dimentional numpy array)
 	(if the image are not regularized to the same size, the output will be a list)
 	'''
-	output = []
+	pic_array = []
 	try:
 		resize = kwargs['resize']
 	except:
@@ -77,10 +81,36 @@ def preprocess(data : 'loaded pic list (from load_pics())',
 		if not(output_format is None):
 			if output_format == '2d':
 				i = i.reshape((i.size))
-		output.append(i)
+		pic_array.append(i)
 	if not(resize is None or color_mode is None):
-		output = np.array(output)
+		pic_array = np.array(pic_array)
+	output = feature_extra(pic_array)
 	return output
+def feature_extra(pics):
+	features = []
+	for i in range(pics.shape[0]): 
+		feature = to_bytearr(pics[i],1)
+		features.append(count_alp(feature,len(pics[i])))
+	return np.array(features)
+
+def to_bytearr(arr,place):
+	bytearr = ''
+	for byte in arr:
+		bytearr += str((byte//place)%2)
+	return bytearr
+
+def count_alp(bits,size):
+	count_arr = []
+	for alp in alpchars:
+		count_arr.append(word_count(bits,alp))
+	count_arr = np.array(count_arr)
+	return count_arr*10/size
+
+def word_count(string, keywords):
+	length = 0
+	for keyword in keywords:
+		length += (len(string.split(keyword))-1)
+	return length
 
 def exe_cmdline():
 	# command line args: imput dir, output csv file name, resize shape
@@ -90,8 +120,8 @@ def main(input,output,resize):
 	# example
 	# da = preprocess(im,resize=(128,128),color_mode='RGB',output_format='2d')
 	im , file_path = load_pics(input)
-	data = preprocess(im,resize=eval(resize),color_mode='RGB',output_format='2d',int_process_RGB=True)
-	data = np.concatenate((np.array(file_path).reshape(-1,1),data),axis=1)
+	data = preprocess(im,resize=eval(resize),color_mode='RGB',output_format='2d',int_process_RGB=False)
+	data = np.concatenate((np.array(file_path)[:,None],data),axis=1)
 	pd.DataFrame(data).to_csv(output,index=False,header=False)
 
 def built_training_set(directory,targetfile,resize,tag):
@@ -104,24 +134,25 @@ def built_training_set(directory,targetfile,resize,tag):
 					file_paths.append(filepath)
 					im.append(Image.open(filepath))
 					if count > 100:
-						data = preprocess(im,resize=resize,color_mode='RGB',output_format='2d',int_process_RGB=True)
-						data = np.concatenate((np.array(file_paths).reshape(-1,1),data),axis=1)
+						data = preprocess(im,resize=resize,color_mode='RGB',output_format='2d',int_process_RGB=False)
+						data = np.concatenate((np.array(file_paths)[:,None],data),axis=1)
 						im , file_paths, count = [], [], 0
 						if tag == 0:
-							data = np.concatenate((data,np.zeros(data.shape[0]).reshape(-1,1)),axis=1)
+							data = np.concatenate((data,np.zeros(data.shape[0])[:,None]),axis=1)
 						elif tag == 1:
-							data = np.concatenate((data,np.ones(data.shape[0]).reshape(-1,1)),axis=1)
+							data = np.concatenate((data,np.ones(data.shape[0])[:,None]),axis=1)
 						pd.DataFrame(data).to_csv(targetfile,index=False,header=False,mode='a')
 					count=count+1
 	if count != 0:
-		data = preprocess(im,resize=resize,color_mode='RGB',output_format='2d',int_process_RGB=True)
-		data = np.concatenate((np.array(file_paths).reshape(-1,1),data),axis=1)
+		data = preprocess(im,resize=resize,color_mode='RGB',output_format='2d',int_process_RGB=False)
+		data = np.concatenate((np.array(file_paths)[:,None],data),axis=1)
 		if tag == 0:
-			data = np.concatenate((data,np.zeros(data.shape[0]).reshape(-1,1)),axis=1)
+			data = np.concatenate((data,np.zeros(data.shape[0])[:,None]),axis=1)
 		elif tag == 1:
-			data = np.concatenate((data,np.ones(data.shape[0]).reshape(-1,1)),axis=1)
+			data = np.concatenate((data,np.ones(data.shape[0])[:,None]),axis=1)
 		pd.DataFrame(data).to_csv(targetfile,index=False,header=False,mode='a')
 	return data
 
 if __name__ == '__main__':
-	exe_cmdline()
+	main('./new','new.csv','(64,64)')
+	#exe_cmdline()
